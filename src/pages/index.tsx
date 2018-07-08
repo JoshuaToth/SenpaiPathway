2f1d51ec6f71012a895200a9c4f7f4a7d6819b2a
Joshua Toth <tothy4@gmail.com>
2018-07-09 08:50:51 +1000

    add tiles for desktop selections

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,18 +31,42 @@ export default class extends React.Component<IndexPageProps, {}> {
           Duis non fermentum metus, ac pellentesque ante. Integer non velit magna. Etiam eu convallis velit, ac laoreet nisl. Nunc non orci non leo ullamcorper posuere a ac est. Nullam dapibus enim et lacus varius, vel semper risus dignissim. Etiam pretium ante ac dui blandit consequat. Fusce consequat lacinia tortor id viverra. Vivamus tincidunt, ex sed dictum eleifend, ligula tortor aliquam mauris, ac ullamcorper magna est eu ante. Morbi ullamcorper, tellus eget ornare iaculis, purus mi facilisis ligula, ut hendrerit tortor lacus id dui. Donec ut libero mi. Phasellus egestas arcu quis erat vehicula varius. Duis bibendum egestas sapien, sit amet interdum nisl lobortis vel. In nec orci at enim iaculis convallis. Ut lacus mi, suscipit ac pellentesque eu, dictum molestie arcu.
           </p>
           <h2>Getting started</h2>
-          <h3>
-            <Link to="/developers/">Developers</Link>
-          </h3>
-          <h3>
-            <Link to="/developers/">Devops</Link>
-          </h3>
-          <h3>
-            <Link to="/developers/">Designers</Link>
-          </h3>
-          <h3>
-            <Link to="/developers/">UX/UI</Link>
-          </h3>
+          <div className='container'>
+            <div className='row'>
+              <div className='col-md-4'>
+                <Link to="/developers/">
+                  <img src={ require('../images/code-coding-computer-34676.jpg') } />
+                  <h3>
+                    Developers
+                  </h3>
+                </Link>
+              </div>
+              <div className='col-md-4'>
+                <Link to="/developers/">
+                  <img src={ require('../images/bandwidth-close-up-computer-1148820.jpg') } />
+                  <h3>
+                    Devops
+                  </h3>
+                </Link>
+              </div>
+              <div className='col-md-4'>
+                <Link to="/developers/">
+                  <img src={ require('../images/apple-computer-decor-326502.jpg') } />
+                  <h3>
+                    Designers
+                  </h3>
+                </Link>
+              </div>
+              <div className='col-md-4'>
+                <Link to="/developers/">
+                  <img src={ require('../images/blur-cellphone-close-up-196644.jpg') } />
+                  <h3>
+                    UX/UI
+                  </h3>
+                </Link>
+              </div>
+            </div>
+          </div>
       </div>
     )
   }
